89631cdde31dcc488afb316be998e5738e4a26f2
SaloniSS <sxs170015@utdallas.edu>
2021-05-06 19:42:06 -0500

    update code

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import base64
 import json
-#discord-webhook>=0.13.0
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 def hello_pubsub(event, context):
@@ -9,7 +8,7 @@ def hello_pubsub(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/821559497147416617/cKmxC5uKk1iTm0_H7Fp7GD3JQevzW2m1GknYu7BTGfk330xE3Z8tzDaCy4OJrMEgF7H-', content='<@&656976951143301166> :money_with_wings: ')
+    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/840023653147082833/fm9pjwaXMV4AGlpDPl6FR4UexhLc2WOEUgXqYXo3aTCSNY2IF_26bovEUyeAUA2mzsT0', content='<@&656976951143301166> :money_with_wings: ')
     response = webhook.execute()
 
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
@@ -18,12 +17,14 @@ def hello_pubsub(event, context):
     webhook = DiscordWebhook(url='https://discord.com/api/webhooks/821559497147416617/cKmxC5uKk1iTm0_H7Fp7GD3JQevzW2m1GknYu7BTGfk330xE3Z8tzDaCy4OJrMEgF7H-')
     embed = DiscordEmbed(title=':bangbang: GCP Budget Alert Threshold Exceeded :bangbang:')
     embed.set_footer(text='GCP Billing Alerts', icon_url='https://cloud.google.com/_static/cloud/images/social-icon-google-cloud-1200-630.png')
+    
     embed.add_embed_field(name='Budget Name:', value=alert["budgetDisplayName"])
     embed.add_embed_field(name="Current Spend:", value=str(alert["costAmount"]))
     embed.add_embed_field(name="Budget Amount Exceeded:", value=str(alert["budgetAmount"]))
     embed.add_embed_field(name="Alert Threshold:", value=str(alert["alertThresholdExceeded"] * 100)+"%")
     embed.add_embed_field(name="Currency Type:", value=alert["currencyCode"])
     embed.add_embed_field(name="Budget Start Date:", value=alert["costIntervalStart"])
+    
     embed.set_timestamp()
 
     webhook.add_embed(embed)
