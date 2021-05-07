3f5cd013ac6c948a474b7b953904b3d947ef9066
Saloni Shivdasani <sxs170015@utdallas.edu>
2021-05-06 22:30:36 -0500

    Update webhook url

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def hello_pubsub(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/840023653147082833/fm9pjwaXMV4AGlpDPl6FR4UexhLc2WOEUgXqYXo3aTCSNY2IF_26bovEUyeAUA2mzsT0', content='<@&656976951143301166> :money_with_wings: ')
+    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/840065577342599198/03UTXcvxESj0JaZ9vC5-gSKaxmW96dQXgToriS84Iwx8Cw0-mdxvXW_tDFn6xYzCGez1', content='<@&656976951143301166> :money_with_wings: ')
     response = webhook.execute()
 
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
     alert = json.loads(pubsub_message)
 
-    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/821559497147416617/cKmxC5uKk1iTm0_H7Fp7GD3JQevzW2m1GknYu7BTGfk330xE3Z8tzDaCy4OJrMEgF7H-')
+    webhook = DiscordWebhook(url='https://discord.com/api/webhooks/840065577342599198/03UTXcvxESj0JaZ9vC5-gSKaxmW96dQXgToriS84Iwx8Cw0-mdxvXW_tDFn6xYzCGez1')
     embed = DiscordEmbed(title=':bangbang: GCP Budget Alert Threshold Exceeded :bangbang:')
     embed.set_footer(text='GCP Billing Alerts', icon_url='https://cloud.google.com/_static/cloud/images/social-icon-google-cloud-1200-630.png')
     
